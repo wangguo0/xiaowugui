bfe5cb825cd2e2fed249640ebe19c73913302773
www.xueximeng.com <admin@xueximeng.com>
2026-06-29 10:42:07 +0800

    Fix mobile playback history save on exit

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -1064,7 +1064,12 @@ public class VideoActivity extends PlaybackActivity implements Clock.Callback, C
 
     private void onBack() {
         if (isFullscreen()) exitFullscreen();
-        else finishPlayback();
+        else finishVideoPlayback();
+    }
+
+    private void finishVideoPlayback() {
+        saveHistory(true);
+        finishPlayback();
     }
 
     private void onCast() {
@@ -2451,6 +2456,7 @@ public class VideoActivity extends PlaybackActivity implements Clock.Callback, C
             exitFullscreen();
         } else if (!isLock()) {
             mViewModel.stopSearch();
+            saveHistory(true);
             markPlaybackExiting();
             stopPlayback();
             if (isTaskRoot()) startActivity(new Intent(this, HomeActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
